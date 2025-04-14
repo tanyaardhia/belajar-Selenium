86acea43407c02d24dd4a703e7639a3e6fe780ed
TanyaArdhia <tanyaardhiap@gmail.com>
2025-04-14 15:55:39 +0700

    feat: test

---
+++ testingSelenium/test.js
@@ -0,0 +1,12 @@
+const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
+
+;(async function example() {
+  let driver = await new Builder().forBrowser("chrome").build()
+  try {
+    await driver.get('https://www.google.com/ncr')
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
+    await driver.wait(until.titleIs('webdriver - Google Search'), 100000)
+  } finally {
+    await driver.quit()
+  }
+})()
